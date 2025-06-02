bc02e0c9a0b605c2540b0b84e743d511ae376f90
Banidu <madmacmester@gmail.com>
2025-06-02 19:15:16 +0200

    -New Node: MergePoints helper that will merge multiple pointbuffers on eachother with blending

---
+++ Operators/Lib/Resources/points/combine/MergePoints.hlsl
@@ -0,0 +1,33 @@
+﻿#include "lib/shared/point.hlsl"
+#include "lib/shared/quat-functions.hlsl"
+cbuffer Params : register(b0)
+{
+    int startIndex;    // 0 3 6
+}
+RWStructuredBuffer<LegacyPoint> PointsBuffer : t0;
+StructuredBuffer<LegacyPoint> Points : t1;            // input
+RWStructuredBuffer<LegacyPoint> ResultPoints : u0;    // output
+
+[numthreads(256,1,1)]
+void main(uint3 i : SV_DispatchThreadID)
+{
+    uint sizea, size, stride;
+    PointsBuffer.GetDimensions(sizea, stride);
+    Points.GetDimensions(size, stride);
+
+    if(i.x > size)  // 9
+        return;
+    
+    if (startIndex == 0) {
+        PointsBuffer[i.x] = Points[i.x];
+    }
+
+    ResultPoints[i.x].Rotation = Points[i.x].Rotation;
+    ResultPoints[i.x].Position = Points[i.x].Position;
+    ResultPoints[i.x].W = max(Points[i.x].Color ,PointsBuffer[i.x].Color);
+    ResultPoints[i.x].Stretch = Points[i.x].Stretch;
+    ResultPoints[i.x].Selected = Points[i.x].Selected;
+    ResultPoints[i.x].Color = max(Points[i.x].Color ,PointsBuffer[i.x].Color);
+    PointsBuffer[i.x] = ResultPoints[i.x];
+
+}
---
+++ Operators/Lib/point/combine/MergePoints.cs
@@ -0,0 +1,29 @@
+﻿namespace Lib.point.combine;
+
+[Guid("7cbb9cab-fa32-46d6-afca-5c198f34bd67")]
+internal sealed class MergePoints : Instance<MergePoints>
+{
+
+    [Output(Guid = "085fdf4a-1bab-496a-b8f0-afb88a0ab511")]
+    public readonly Slot<BufferWithViews> OutBuffer = new();
+
+        [Input(Guid = "4a8e200c-32c0-4c5a-b56d-ddc2994e7b99")]
+        public readonly MultiInputSlot<T3.Core.DataTypes.BufferWithViews> Input = new MultiInputSlot<T3.Core.DataTypes.BufferWithViews>();
+
+        
+    private enum BlendModes
+    {
+        Mix,
+        UseA_F1,
+        UseB_F1,
+        MixWithSlidingRange,
+        MixWithSlidingRangeSmooth,
+    }
+        
+    private enum PairingModes
+    {
+        WrapAround,
+        Adjust,
+    }
+        
+}
